5544239f7f2f8f9b83dfa3fa5174e545dff83330
Vedant Games <guptavedant2549@gmail.com>
2024-04-03 16:41:03 +0530

    updates

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ console.log("started..+.", __dirname);
 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:5173',
 }));
 
 mongoose.connect(process.env.MONGO_URL);
